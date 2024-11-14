39b470f9a43ecdde2f2d8f71721da3ae0abb6569
Miha <stele1000@gmail.com>
2024-11-14 15:59:50 +0100

    Refactor

--- parser.py
+++ parser.py
@@ -67,28 +67,28 @@ class Parser:
             return UnOp(op, operand, self.peek().line)
         return self.primary()
 
-    # <factor>  ::=  <unary>
-    def factor(self):
-        return self.unary()
 
-    # <term>  ::=  <factor> ( ('*'|'/') <factor> )*
-    def term(self):
-        expr = self.factor()
+    # <multiplication>  ::=  <unary> ( ('*'|'/') <unary> )*
+    def multiplication(self):
+        expr = self.unary()
         while self.match(TOK_STAR) or self.match(TOK_SLASH):
             op = self.previous_token()
-            right = self.factor()
+            right = self.unary()
             # print(f"Binary operation: {op.lexeme}")
             expr = BinOp(op, expr, right, self.peek().line)
         return expr
 
-    # <expr>  ::=  <term> ( ('+'|'-') <term> )*
-    def expr(self):
-        expr = self.term()
+    # <addition>  ::=  <multiplication> ( ('+'|'-') <multiplication> )*
+    def addition(self):
+        expr = self.multiplication()
         while self.match(TOK_PLUS) or self.match(TOK_MINUS):
             op = self.previous_token()
-            right = self.term()
+            right = self.multiplication()
             expr = BinOp(op, expr, right, self.peek().line)
         return expr
+    
+    def expr(self):
+        return self.addition()
 
     def parse(self):
         ast = self.expr()
